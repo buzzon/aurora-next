16498396f00145272619fb5af184c7fe81b1e52f
buzzon <buzzondev@gmail.com>
2021-08-12 10:41:41 +0500

    remove typesctipt

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
-function Home() {
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
@@ -23,5 +23,3 @@ function Home() {
     </div>
   )
 }
-
-export default Home
