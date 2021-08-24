6741eb3a9d16938eecf001d6d94b1786b3fd2d88
buzzon <buzzondev@gmail.com>
2021-08-24 16:41:20 +0500

    update form

--- pages/calendar/index.js
+++ pages/calendar/index.js
@@ -1,16 +1,16 @@
 import Head from 'next/head'
 import styles from '../../styles/Calendar.module.css'
-import { Calendar, Tag, Modal, Form, Input, Radio, Button, Space  } from 'antd';
-import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+import { Row, Col, Calendar, Select, Tag, Modal, Form, Input, Divider, Button } from 'antd';
+import { MinusCircleTwoTone, PlusOutlined } from '@ant-design/icons';
 import cookieCutter from 'cookie-cutter'
 import moment from 'moment';
 
 import React, { useState, useEffect } from 'react';
 
-function eventsToMap(events){
+function eventsToMap(events) {
   const map = new Map()
   events.forEach(element => {
-    if (map.has(element.date)){
+    if (map.has(element.date)) {
       map.get(element.date).push(element)
     }
     else
@@ -43,11 +43,11 @@ function getLabels(events) {
 
 const EventsCreateForm = ({ visible, curentMoment, curent_events, onFinish, onCancel }) => {
   const [form] = Form.useForm();
-  useEffect(() => { 
+  useEffect(() => {
     form.resetFields();
     form.setFieldsValue(curent_events);
   }, [form, curent_events])
-  
+
   const { TextArea } = Input;
   return (
     <Modal
@@ -62,6 +62,7 @@ const EventsCreateForm = ({ visible, curentMoment, curent_events, onFinish, onCa
           .then((values) => {
             form.resetFields();
             onFinish(values);
+            console.log('values', values);
           })
           .catch((info) => {
             console.warn('Validate Failed:', info);
@@ -72,47 +73,60 @@ const EventsCreateForm = ({ visible, curentMoment, curent_events, onFinish, onCa
         form={form}
         layout="vertical"
         name="form_in_modal"
-        initialValues={{events: curent_events}}
+        initialValues={{ events: curent_events }}
       >
         <Form.List name="events">
-        {(events, { add, remove }) => (
-          <>
-            {events.map(({ key, name, fieldKey, ...restField }) => (
-              <Space key={key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
-                <Form.Item
-                  {...restField}
-                  name={[name, 'description']}
-                  rules={[{ message: 'Missing first name' }]}
-                >
-                  <TextArea placeholder="Description"  />
-                </Form.Item>
-                <Form.Item
-                  {...restField}
-                  name={[name, 'first']}
-                  fieldKey={[fieldKey, 'first']}
-                  rules={[{ required: true, message: 'Missing first name' }]}
-                >
-                  <Input placeholder="First Name" />
-                </Form.Item>
-                <Form.Item
-                  {...restField}
-                  name={[name, 'last']}
-                  fieldKey={[fieldKey, 'last']}
-                  rules={[{ required: true, message: 'Missing last name' }]}
-                >
-                  <Input placeholder="Last Name" />
-                </Form.Item>
-                <MinusCircleOutlined onClick={() => remove(name)} />
-              </Space>
-            ))}
-            <Form.Item>
-              <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
-                Add field
-              </Button>
-            </Form.Item>
-          </>
-        )}
-      </Form.List>
+          {(events, { add, remove }) => (
+            <>
+              {events.map(({ key, name, ...restField }) => (
+                <Row key={key} justify="space-between" align="middle">
+                  <Col span={22}>
+                    <Form.Item>
+                      <Input.Group compact>
+                        <Form.Item
+                          name={[name, 'label', 'title']}
+                          noStyle
+                          rules={[{ message: 'Label is required' }]}
+                        >
+                          <Select 
+                            placeholder="Select label"
+                            style={{ width: '80%' }}
+                          >
+                            <Option value="Zhejiang">Zhejiang</Option>
+                            <Option value="Jiangsu">Jiangsu</Option>
+                          </Select>
+                        </Form.Item>
+                        <Form.Item
+                          name={[name, 'label', 'color']}
+                          noStyle
+                          rules={[{ message: 'Select color' }]}
+                        >
+                          <Input style={{ width: '20%' }} placeholder="Input street" />
+                        </Form.Item>
+                      </Input.Group>
+                    </Form.Item>
+                    <Form.Item
+                      {...restField}
+                      noStyle
+                      name={[name, 'description']}
+                    >
+                      <TextArea placeholder="Description" />
+                    </Form.Item>
+                  </Col>
+                  <Col span={1} offset={1}>
+                    <MinusCircleTwoTone onClick={() => remove(name)} />
+                  </Col>
+                  <Divider />
+                </Row>
+              ))}
+              <Form.Item>
+                <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                  Add field
+                </Button>
+              </Form.Item>
+            </>
+          )}
+        </Form.List>
       </Form>
     </Modal>
   );
@@ -125,7 +139,7 @@ function CalendarPage() {
   const [events, setEvents] = useState(new Map())
   const [curent_events, setCeurentEvents] = useState([])
   const [isNeedRerende, setIsNeedRerende] = useState(Math.random())
-  
+
   useEffect(() => {
     fetch('http://127.0.0.1:8000/schedule/api/events/', {
       method: 'GET',
@@ -134,27 +148,27 @@ function CalendarPage() {
         'Authorization': `token ${cookieCutter.get('Token')}`
       },
     })
-    .then(res => res.json())
-    .then(data => { setEvents(eventsToMap(data)); setLabels(getLabels(data))})
+      .then(res => res.json())
+      .then(data => { setEvents(eventsToMap(data)); setLabels(getLabels(data)) })
   }, [isNeedRerende])
-  
+
   if (!events) return <Spinner />
-  
+
   const reRender = () => setIsNeedRerende(Math.random())
-  
+
   function dateCellRender(value) {
     return (
       <div className={styles.day_events}>
-        { (events.get(formatDate(value._d)) || [] ).map(event => (
+        {(events.get(formatDate(value._d)) || []).map(event => (
           <span key={event.id} style={{ background: event.label.color }} className={styles.event} />
-        )) }
+        ))}
       </div>
     );
   }
 
-  function onSelect(value){
-    if (value.format("MMM-YY") == curentMoment.format("MMM-YY")){
-      setCeurentEvents(events.get(formatDate(value._d)) || [] )
+  function onSelect(value) {
+    if (value.format("MMM-YY") == curentMoment.format("MMM-YY")) {
+      setCeurentEvents(events.get(formatDate(value._d)) || [])
       setVisible(true)
     }
     setMoment(value)
@@ -175,7 +189,7 @@ function CalendarPage() {
           <div className={styles.calendar_card}>
             <Calendar dateCellRender={dateCellRender} fullscreen={false} onPanelChange={reRender} onSelect={onSelect} />
           </div>
-          <br/>
+          <br />
           <div>
             {labels.map(label => (
               <Tag key={label.id} color={label.color}>{label.title}</Tag>
